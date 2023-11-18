be71989a25db123f95efc3f7f291d9b07dae4646
Ahmed Sayed <therealahme@gmail.com>
2023-11-18 10:01:00 +0200

    refactor some code

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
 let history = [];
+const MAX_INPUT_DIGITS = 14;
 
-function appendToDisplay(value) {
-  document.getElementById('display').value += value;
-}
+document.getElementById('calculator-buttons').addEventListener('click', function (event) {
+  const target = event.target;
+  if (target.tagName === 'BUTTON') {
+    const buttonValue = target.dataset.value;
+    const buttonAction = target.dataset.action;
 
-function clearDisplay() {
-  document.getElementById('display').value = '';
-}
-
-function calculateResult() {
-  try {
-    const result = eval(document.getElementById('display').value);
-    document.getElementById('display').value = result;
-    history.push(`${document.getElementById('display').value} = ${result}`);
-    updateHistory();
-  } catch (error) {
-    document.getElementById('display').value = 'Error';
+    if (buttonAction === 'clear') {
+      clearDisplay();
+    } else if (buttonAction === 'calculate') {
+      calculateResult();
+    } else if (buttonAction === 'sqrt') {
+      calculateSquareRoot();
+    } else {
+      appendToDisplay(buttonValue);
+    }
   }
-}
-
-function calculateSquareRoot() {
-  document.getElementById('display').value = Math.sqrt(eval(document.getElementById('display').value));
-}
-
-function updateHistory() {
-  const historyList = document.getElementById('history-list');
-  historyList.innerHTML = '';
-  history.forEach(entry => {
-    const li = document.createElement('li');
-    li.textContent = entry;
-    historyList.appendChild(li);
-  });
-}
+});
 
 document.addEventListener('keydown', function (event) {
   const key = event.key;
-  if (/^[0-9+\-*/.C=]$/.test(key)) {
+  if (/^[0-9+\-*/.=]$/.test(key)) {
     event.preventDefault();
     if (key === 'C') {
       clearDisplay();
@@ -46,3 +32,66 @@ document.addEventListener('keydown', function (event) {
     }
   }
 });
+
+function appendToDisplay(value) {
+  const currentInput = document.getElementById('display').value;
+  if (currentInput.length < MAX_INPUT_DIGITS) {
+    document.getElementById('display').value += value;
+  } else {
+    displayMessage('Maximum input limit reached (14 digits)');
+  }
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+  clearMessage();
+}
+
+function calculateResult() {
+  try {
+    const result = eval(document.getElementById('display').value);
+    const formattedResult = formatResult(result);
+    document.getElementById('display').value = formattedResult;
+    history.push(`${document.getElementById('display').value} = ${formattedResult}`);
+    updateHistory();
+    clearDisplayOnOperator();
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
+
+function clearDisplayOnOperator() {
+  const currentInput = document.getElementById('display').value;
+  const operators = ['+', '-', '*', '/'];
+
+
+  if (operators.some(operator => currentInput.endsWith(operator))) {
+    clearDisplay();
+  }
+}
+
+function calculateSquareRoot() {
+  document.getElementById('display').value = Math.sqrt(eval(document.getElementById('display').value));
+}
+
+function formatResult(result) {
+  return Number(result.toPrecision(MAX_INPUT_DIGITS)).toString();
+}
+
+function updateHistory() {
+  const historyList = document.getElementById('history-list');
+  historyList.innerHTML = '';
+  history.forEach((entry) => {
+    const li = document.createElement('li');
+    li.textContent = entry;
+    historyList.appendChild(li);
+  });
+}
+
+function displayMessage(message) {
+  document.getElementById('display').value = message;
+}
+
+function clearMessage() {
+  document.getElementById('display').value = '';
+}
