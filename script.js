be5ecfc08ba5f26b289dd3db558f8c197f05dc49
Ahmed Sayed <therealahme@gmail.com>
2023-11-18 09:37:38 +0200

    add history functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let history = [];
+
 function appendToDisplay(value) {
   document.getElementById('display').value += value;
 }
@@ -8,7 +10,10 @@ function clearDisplay() {
 
 function calculateResult() {
   try {
-    document.getElementById('display').value = eval(document.getElementById('display').value);
+    const result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+    history.push(`${document.getElementById('display').value} = ${result}`);
+    updateHistory();
   } catch (error) {
     document.getElementById('display').value = 'Error';
   }
@@ -17,3 +22,27 @@ function calculateResult() {
 function calculateSquareRoot() {
   document.getElementById('display').value = Math.sqrt(eval(document.getElementById('display').value));
 }
+
+function updateHistory() {
+  const historyList = document.getElementById('history-list');
+  historyList.innerHTML = '';
+  history.forEach(entry => {
+    const li = document.createElement('li');
+    li.textContent = entry;
+    historyList.appendChild(li);
+  });
+}
+
+document.addEventListener('keydown', function (event) {
+  const key = event.key;
+  if (/^[0-9+\-*/.C=]$/.test(key)) {
+    event.preventDefault();
+    if (key === 'C') {
+      clearDisplay();
+    } else if (key === '=') {
+      calculateResult();
+    } else {
+      appendToDisplay(key);
+    }
+  }
+});
